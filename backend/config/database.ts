cf81c71d23ac49de7d449cef546272a8ea8baecf
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-01-22 18:51:42 +0530

    1.10

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL!);
+  await mongoose.connect(process.env.MONGO_URL!,{
+    tls: true,
+  tlsAllowInvalidCertificates: false
+  });
   console.log("MongoDB connected");
 };
